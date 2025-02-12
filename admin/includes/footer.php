e2c345038a7139890788f26ada0a650142e99c57
srijanbea <srijanadhikari300@gmail.com>
2025-02-12 08:38:35 +0545

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2025 <a href="https://www.nid.com.np">NID Payroll</a></strong>
+</footer>
