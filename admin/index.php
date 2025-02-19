7d0663a440d3b4ccad28d813f4de49c08276090c
srijanbea <srijanadhikari300@gmail.com>
2025-02-19 12:07:03 +0545

    fianal comitt

--- admin/index.php
+++ admin/index.php
@@ -5,43 +5,46 @@
   }
 ?>
 <?php include 'includes/header.php'; ?>
-<body class="hold-transition login-page">
-<div class="login-box">
-  	<div class="login-logo">
-  		<b>Admin Login</b>
-  	</div>
+<body class="hold-transition login-page" style="background: linear-gradient(135deg, #667eea, #764ba2);">
+<div class="login-box" style="border-radius: 10px; box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3); background: white; padding: 20px;">
+   <div class="login-logo">
+   <img src="https://w7.pngwing.com/pngs/473/130/png-transparent-payroll-business-human-resource-logo-sales-business-text-service-people-thumbnail.png" style="width: 300px; border-radius: 0%;">
+      <b style="color: #333; font-size: 24px;">Admin Login</b>
+   </div>
   
-  	<div class="login-box-body">
-    	<p class="login-box-msg">Sign in to start your session</p>
+   <div class="login-box-body">
+      <p class="login-box-msg" style="font-size: 16px; color: #555;">Sign in to start your session</p>
 
-    	<form action="login.php" method="POST">
-      		<div class="form-group has-feedback">
-        		<input type="text" class="form-control" name="username" placeholder="input Username" required autofocus>
-        		<span class="glyphicon glyphicon-user form-control-feedback"></span>
-      		</div>
-          <div class="form-group has-feedback">
-            <input type="password" class="form-control" name="password" placeholder="input Password" required>
-            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      <form action="login.php" method="POST">
+        <div class="form-group has-feedback">
+          <input type="text" class="form-control input-lg" name="username" placeholder="Username" required autofocus style="border-radius: 5px;">
+          <span class="glyphicon glyphicon-user form-control-feedback"></span>
+        </div>
+        <div class="form-group has-feedback">
+          <input type="password" class="form-control input-lg" name="password" placeholder="Password" required style="border-radius: 5px;">
+          <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+        </div>
+        <div class="row">
+          <div class="col-xs-12 text-center">
+            <button type="submit" class="btn btn-primary btn-block btn-lg" name="login" style="border-radius: 5px; font-size: 18px; font-weight: bold; background: #4CAF50; border: none; padding: 10px;">
+              <i class="fa fa-sign-in"></i> Sign In
+            </button>
           </div>
-      		<div class="row">
-    			<div class="col-xs-4">
-          			<button type="submit" class="btn btn-primary btn-block btn-flat" name="login"><i class="fa fa-sign-in"></i> Sign In</button>
-        		</div>
-      		</div>
-    	</form>
-  	</div>
-  	<?php
-  		if(isset($_SESSION['error'])){
-  			echo "
-  				<div class='callout callout-danger text-center mt20'>
-			  		<p>".$_SESSION['error']."</p> 
-			  	</div>
-  			";
-  			unset($_SESSION['error']);
-  		}
-  	?>
+        </div>
+      </form>
+   </div>
+   <?php
+      if(isset($_SESSION['error'])){
+        echo "
+          <div class='callout callout-danger text-center mt20' style='color: red; font-weight: bold;'>
+            <p>".$_SESSION['error']."</p> 
+          </div>
+        ";
+        unset($_SESSION['error']);
+      }
+   ?>
 </div>
-	
+  
 <?php include 'includes/scripts.php' ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,78 +1,88 @@
 <?php session_start(); ?>
 <?php include 'header.php'; ?>
-<body class="hold-transition login-page">
-<div class="login-box">
-  	<div class="login-logo">
-  		<p id="date"></p>
-      <p id="time" class="bold"></p>
-  	</div>
-  
-  	<div class="login-box-body">
-    	<h4 class="login-box-msg">Enter Employee ID</h4>
+<body class="hold-transition login-page" style="background: linear-gradient(45deg,rgb(212, 212, 212),rgb(255, 255, 255));">
+  <div class="login-box">
+    <div class="login-logo">
+      <img src="images\logo.png" alt="Company Logo" style="width: 300px; border-radius: 0%;">
+      <h1 class="text-white">Welcome to Attendance Portal</h1>
+      <p id="date" class="text-white"></p>
+      <p id="time" class="bold text-white"></p>
+    </div>
 
-    	<form id="attendance">
-          <div class="form-group">
-            <select class="form-control" name="status">
-              <option value="in">Time In</option>
-              <option value="out">Time Out</option>
-            </select>
+    <div class="login-box-body" style="border-radius: 10px; box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2); background: white;">
+      <h4 class="login-box-msg"><i class="fa fa-user"></i> Enter Employee ID</h4>
+      <form id="attendance">
+        <div class="form-group">
+          <label><i class="fa fa-clock-o"></i> Select Status</label>
+          <select class="form-control" name="status">
+            <option value="in">Time In</option>
+            <option value="out">Time Out</option>
+          </select>
+        </div>
+        <div class="form-group has-feedback">
+          <label><i class="fa fa-id-card"></i> Employee ID</label>
+          <input type="text" class="form-control input-lg" id="employee" name="employee" required>
+          <span class="glyphicon glyphicon-calendar form-control-feedback"></span>
+        </div>
+        <div class="row">
+          <div class="col-xs-4">
+            <button type="submit" class="btn btn-primary btn-block btn-flat" name="signin">
+              <i class="fa fa-sign-in"></i> Sign In
+            </button>
           </div>
-      		<div class="form-group has-feedback">
-        		<input type="text" class="form-control input-lg" id="employee" name="employee" required>
-        		<span class="glyphicon glyphicon-calendar form-control-feedback"></span>
-      		</div>
-      		<div class="row">
-    			<div class="col-xs-4">
-          			<button type="submit" class="btn btn-primary btn-block btn-flat" name="signin"><i class="fa fa-sign-in"></i> Sign In</button>
-        		</div>
-      		</div>
-    	</form>
-  	</div>
-		<div class="alert alert-success alert-dismissible mt20 text-center" style="display:none;">
+        </div>
+      </form>
+    </div>
+    <div class="alert alert-success alert-dismissible mt20 text-center" style="display:none;">
       <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
       <span class="result"><i class="icon fa fa-check"></i> <span class="message"></span></span>
     </div>
-		<div class="alert alert-danger alert-dismissible mt20 text-center" style="display:none;">
+    <div class="alert alert-danger alert-dismissible mt20 text-center" style="display:none;">
       <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
       <span class="result"><i class="icon fa fa-warning"></i> <span class="message"></span></span>
     </div>
-  		
-</div>
-	
-<?php include 'scripts.php' ?>
-<script type="text/javascript">
-$(function() {
-  var interval = setInterval(function() {
-    var momentNow = moment();
-    $('#date').html(momentNow.format('dddd').substring(0,3).toUpperCase() + ' - ' + momentNow.format('MMMM DD, YYYY'));  
-    $('#time').html(momentNow.format('hh:mm:ss A'));
-  }, 100);
+  </div>
+  <div class="text-center mt-4">
+  <div class="text-center mt-3">
+        <button onclick="window.open('admin', '_blank')" class="btn btn-dark" style="padding: 10px 20px; border-radius: 5px; font-weight: bold;">
+          <i class="fa fa-cog"></i> Go to Admin Panel
+        </button>
+      </div>
+    </div>
 
-  $('#attendance').submit(function(e){
-    e.preventDefault();
-    var attendance = $(this).serialize();
-    $.ajax({
-      type: 'POST',
-      url: 'attendance.php',
-      data: attendance,
-      dataType: 'json',
-      success: function(response){
-        if(response.error){
-          $('.alert').hide();
-          $('.alert-danger').show();
-          $('.message').html(response.message);
-        }
-        else{
-          $('.alert').hide();
-          $('.alert-success').show();
-          $('.message').html(response.message);
-          $('#employee').val('');
-        }
-      }
+  <?php include 'scripts.php' ?>
+  <script type="text/javascript">
+    $(function() {
+      var interval = setInterval(function() {
+        var momentNow = moment();
+        $('#date').html(momentNow.format('dddd').substring(0,3).toUpperCase() + ' - ' + momentNow.format('MMMM DD, YYYY'));
+        $('#time').html(momentNow.format('hh:mm:ss A'));
+      }, 100);
+
+      $('#attendance').submit(function(e){
+        e.preventDefault();
+        var attendance = $(this).serialize();
+        $.ajax({
+          type: 'POST',
+          url: 'attendance.php',
+          data: attendance,
+          dataType: 'json',
+          success: function(response){
+            if(response.error){
+              $('.alert').hide();
+              $('.alert-danger').show();
+              $('.message').html(response.message);
+            }
+            else{
+              $('.alert').hide();
+              $('.alert-success').show();
+              $('.message').html(response.message);
+              $('#employee').val('');
+            }
+          }
+        });
+      });
     });
-  });
-    
-});
-</script>
+  </script>
 </body>
 </html>
